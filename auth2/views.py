5b0ec43374e3fa21b77d7feb7fe7988fe99f2568
Zouhair <h.mohamed.zouhair@gmail.com>
2023-03-06 10:23:26 +0100

    crsf_exempt

--- auth2/views.py
+++ auth2/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def register(request):
 
     if request.method == "POST":
