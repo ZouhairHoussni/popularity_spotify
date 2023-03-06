35454d4a27c4e598f5b70551d216bb486699dcb1
Zouhair <h.mohamed.zouhair@gmail.com>
2023-03-06 11:31:16 +0100

    renable middleware 2

--- spotify_django/settings.py
+++ spotify_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4mre5^v0d1$^7z89cddxn7$wqne=rjcl&)xu(@8cs!7tf54_a*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,7 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
